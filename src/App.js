bd1266d19848140d86a8786970203a8e590ea991
Thiago Diniz <thiago.diniz@apllos.com>
2020-06-25 11:17:17 -0300

    ProgressLine

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './components/Header/Header';
+import ProgressLine from './components/ProgessLine/ProgressLine';
 
 function App() {
   return (
     <div>
       <Header />
+      <ProgressLine />
       <h1>Nostradamus</h1>
     </div>
   );
