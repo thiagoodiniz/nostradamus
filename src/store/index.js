f152849ea3db213ef52a53b9bb228ef8a81cce6c
Thiago Diniz <thiago.diniz@apllos.com>
2020-06-26 16:13:02 -0300

    Adding redux-logger

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,10 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import reducers from './reducers';
+import { logger } from 'redux-logger';
 
 const store = createStore(
     reducers,
+    applyMiddleware(logger)
 );
 
 export default store;
