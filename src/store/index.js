d5cb8012bd902755714c900e3bd04ede22d64165
Thiago Diniz <thiago.diniz@apllos.com>
2020-07-23 16:10:44 -0300

    installing axios and redux-saga

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,16 @@
 import { createStore, applyMiddleware } from 'redux';
 import reducers from './reducers';
 import { logger } from 'redux-logger';
+import createSagaMiddleware from 'redux-saga';
+import sagas from './sagas';
+
+const sagaMiddleware = createSagaMiddleware();
 
 const store = createStore(
     reducers,
-    applyMiddleware(logger)
+    applyMiddleware(logger, sagaMiddleware)
 );
+
+sagaMiddleware.run(sagas);
 
 export default store;
